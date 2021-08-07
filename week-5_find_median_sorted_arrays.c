358c5bd923d6ffb8893877b6b715f28939dba740
Alpaca at home <tim.891020@gmail.com>
2021-08-08 00:56:34 +0800

    not mush change

--- week-5_find_median_sorted_arrays.c
+++ week-5_find_median_sorted_arrays.c
@@ -4,7 +4,19 @@
 #include <string.h>
 
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
-	
+	if(nums1Size>nums2Size) {
+		int temp = nums1Size;
+		nums1Size = nums2Size;
+		nums2Size = temp;
+		int *temp_ = nums1;
+		nums1 = nums2;
+		nums2 = temp_;
+	}
+	int left1=0,left2=0,right1=nums1-1,right2=nums2-2,n1,n2;
+	while(true) {
+		n1=nums1[(left1+right1)/2];
+		n2=nums2[(left2+right2)/2];
+	}
 }
 
 int main() {
